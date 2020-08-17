969014a0a7431ed9c2264cbda45ad97f2f5b07f0
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-17 21:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 
 const findMatching = (drivers, string) => drivers.filter( d => d.toLowerCase() === string.toLowerCase());
+const fuzzyMatch = (drivers, string) => drivers.filter( d => d.slice(0, string.length) === string);
+const matchName = (drivers, string) => drivers.filter( d => d.name === string);
